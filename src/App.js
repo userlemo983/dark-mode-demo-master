5aec39ba523078236409ff65306966f7cac11286
userlemo983 <138650323+userlemo983@users.noreply.github.com>
2023-08-03 15:19:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
 import './App.css';
-import { Container } from 'reactstrap';
-import { ThemeContext, themes } from './contexts/ThemeContext';
-import React from 'react';
-import ToggleDark from './components/toggleDark';
+import PostForm from './components/PostForm';
 
 function App() {
-  const [darkMode, setDarkMode] = React.useState(true);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="text-warning">Dark/Light mode</h1>
-      </header>
-      <ThemeContext.Consumer>
-        {({ changeTheme }) => (
-          <ToggleDark
-            toggleDark={() => {
-              setDarkMode(!darkMode);
-              changeTheme(darkMode ? themes.light : themes.dark);
-            }}
-          />
-        )}
-      </ThemeContext.Consumer>
-
-      <footer className="center footer">
-        <Container>
-            Nigth Mode Toggle Button
-        </Container>
-      </footer>
+      <PostForm />
     </div>
   );
 }
